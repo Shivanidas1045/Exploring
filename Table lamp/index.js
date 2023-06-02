01f057698ee2610dd785927cc66c8bff3c2acca8
Shivani Das <dasshivani313@gmail.com>
2023-06-03 00:15:54 +0530

    table lamp done

---
+++ Table lamp/index.js
@@ -0,0 +1,11 @@
+ function onbutton(){
+    document.getElementById("head").style.borderBottomColor=  "yellow";
+    
+ }
+ function offbutton(){
+    document.getElementById("head").style.borderBottomColor="gray";
+
+
+ }
+//  background-color
+
